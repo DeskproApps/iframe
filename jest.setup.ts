678ca5f65b5d410621dfb6a5b47a3b9d418008da
Tweeno <itweeno@gmail.com>
2023-02-27 15:54:26 +0000

    commit

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,5 +4,6 @@ import { TextDecoder, TextEncoder } from "util";
 
 global.TextEncoder = TextEncoder;
 //for some reason the types are wrong, but this works
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 global.TextDecoder = TextDecoder;
