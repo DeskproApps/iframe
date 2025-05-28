d08b23da244ed73bfd850a505df04c5bc98ee258
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 15:15:46 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import * as Sentry from '@sentry/react';
 import './instrument';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "./main.css";
+import { reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render(
   <React.StrictMode>
     <DeskproAppProvider>
